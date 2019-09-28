8321dee6d39fe03d03f2b62597c8829f3b5ffc94
ilovechina9999999 <ilovechina9999999@gmail.com>
2019-09-28 15:45:20 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ set -e
 cd dist/spa
 
 # if you are deploying to a custom domain
-echo 'www.138921jkf.tk' > CNAME
+echo 'www.mylifeinn.tk' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ilovechina9999999/vue-toolbox.git master:gh-pages
+git push -f git@github.com:i-love-vue/vue-toolbox.git master:gh-pages
 
 cd -
